d4e95d9b6ad04de4c2dae0c7fadd51dc6ca29fc6
DalviBhavesh <dalvibhavesh007@gmail.com>
2024-05-28 13:20:18 +0530

    bug fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -73,7 +73,7 @@ let AmPm = (unix_timestamp)=>{
                     <h1 className="text-lg text font-bold ">
                         {weather&&weather.cod == 200 ? place.toUpperCase(): "Invalid Search"}
                     </h1>
-                    <div className="text-lg  text  h-7 w-10 flex justify-center item-center bg-black text-white rounded-full ">
+                    <div className="text-lg  text  h-7 w-10 flex justify-center item-center bg-cyan-300 border border-black shadow text-black rounded-full ">
                         <h1>{weather&&weather.cod == 200 ? weather.sys.country: "?"}</h1>
                     </div>
                     
